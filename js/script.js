fd7f4426861e08e7fdf97bb31a1be8f0b025ab8e
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-08 16:34:20 +0100

    feat: add function API

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+// FUNCTIONS
+function randomMails() {
+    for (let i = 0; i <= 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            const mailGenerator = response.data.response
+            console.log(mailGenerator);
+        })
+    }
+}
+
 // DOM
 const btnRndMailElm = document.getElementById('get-rnd-mail-btn')
 
-for (let i = 0; i <= 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(response => {
-        console.log(response.data.response);
-        
-    })
-}
+// EVENTS
+btnRndMailElm.addEventListener("click", randomMails)
 
 
