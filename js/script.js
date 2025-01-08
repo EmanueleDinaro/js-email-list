276217418bf69f2f03ee1533ebfd0cd30d2a7381
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-08 17:04:13 +0100

    feat add: innerHTML and reset mail feature

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // FUNCTIONS
 function randomMails() {
-    for (let i = 0; i <= 10; i++) {
+    emailListElm.innerHTML = '';
+    for (let i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(response => {
             const mailGenerator = response.data.response
-            console.log(mailGenerator);
+            emailListElm.innerHTML += `<li>${mailGenerator}</li>`
         })
     }
 }
 
 // DOM
 const btnRndMailElm = document.getElementById('get-rnd-mail-btn')
+const emailListElm = document.getElementById('email-list')
 
 // EVENTS
 btnRndMailElm.addEventListener("click", randomMails)
-
-
