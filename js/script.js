ab4f12091349abaf0c562761ebf5bc26c21bab7d
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-08 16:29:33 +0100

    feat: add axios API & for cicle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+// DOM
+const btnRndMailElm = document.getElementById('get-rnd-mail-btn')
+
+for (let i = 0; i <= 10; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => {
+        console.log(response.data.response);
+        
+    })
+}
+
 
